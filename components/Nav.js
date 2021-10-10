08caed2242b51cd0a1ff460a24b628f93130eaf4
Rajat Bhatt <rajat.bhatt@yatilabs.com>
2021-10-10 14:19:31 +0530

    completed

--- components/Nav.js
+++ components/Nav.js
@@ -1,10 +1,13 @@
+import { useRouter } from "next/dist/client/router";
 import requests from "../utils/requests";
 const Nav = () => {
+  const router = useRouter();
   return (
-    <nav>
+    <nav className="relative">
       <div className="flex px-10 sm:px-20 text-2xl whitespace-nowrap space-x-10 sm:space-x-20 overflow-x-scroll scrollbar-hide">
         {Object.entries(requests).map(([key, { title, url }]) => (
           <h2
+            onClick={() => router.push(`/?genre=${key}`)}
             className="last:pr-24 cursor-poniter transition duration-100 transform hover:scale-125 hover:text-white active:text-red-500"
             key={key}
           >
@@ -12,6 +15,7 @@ const Nav = () => {
           </h2>
         ))}
       </div>
+      <div className="absolute top-0 right-0 bg-gradient-to-l from-[#06202A] h-10 w-1/12" />
     </nav>
   );
 };
--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,11 @@
 import Head from "next/head";
 import Header from "../components/Header";
 import Nav from "../components/Nav";
+import Results from "../components/Results";
+import requests from "../utils/requests";
 
-export default function Home() {
+export default function Home({ results }) {
+  //console.log(props);
   return (
     <div>
       <Head>
@@ -10,11 +13,23 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
-      {/* Header */}
       <Header />
-      {/* Nav */}
       <Nav />
-      {/* Results */}
+      <Results results={results} />
     </div>
   );
+}
+
+export async function getServerSideProps(context) {
+  const genre = context.query.genre;
+  const request = await fetch(
+    `https://api.themoviedb.org/3${
+      requests[genre]?.url || requests.fetchTrending.url
+    }`
+  ).then((res) => res.json());
+  return {
+    props: {
+      results: request.results,
+    },
+  };
 }
